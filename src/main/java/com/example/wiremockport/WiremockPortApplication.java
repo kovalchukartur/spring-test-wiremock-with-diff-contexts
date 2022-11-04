00d11196d62229328f531194095dc43eeeb8204b
Artur Kovalchuk <arkovalchuk@playtika.com>
2022-11-04 19:51:36 +0200

    Init project

---
+++ src/main/java/com/example/wiremockport/WiremockPortApplication.java
@@ -0,0 +1,13 @@
+package com.example.wiremockport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiremockPortApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WiremockPortApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wiremockport/WiremockPortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wiremockport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiremockPortApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
